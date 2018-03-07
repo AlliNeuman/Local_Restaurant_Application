3e36896f70f261e8bad1498b14806d209f99286f
Allison Tzadok <allisontzadok@gmail.com>
2018-03-07 22:53:36 +0000

    added 3 users into the seeds.rb file

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.create(username: "Colin", email: "colin@colin.com", password: "colinpw")
+User.create(username: "Dave", email: "dave@dave.com", password: "davepw")
+User.create(username: "Stacy", email: "stacy@stacy.com", password: "stacypw")
