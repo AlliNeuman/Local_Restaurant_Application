79a210a3a1e73be0ee80c146863b15384385af7c
Allison Tzadok <allisontzadok@gmail.com>
2018-03-12 00:20:54 +0000

    worked on user controller and slug. still have issues to fix. page not loading correctly.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,72 +1,74 @@
 class UserController < ApplicationController
 
-  get '/users' do
-    if logged_in?
-      @creators = User.all
-      erb :'users/index'
-    else
-      flash[:message] = "Please log in to view users."
-      redirect to '/login'
-    end
-  end
+  # get '/users' do
+  #   if logged_in?
+  #     @creators = User.all
+  #     erb :'users/index'
+  #   else
+  #     flash[:message] = "Please log in to view users."
+  #     redirect to '/login'
+  #   end
+  # end
 
-  get '/users/:slug' do
-    if logged_in?
-      @creator = User.find_by_slug(params[:slug])
-      erb :'users/show'
-    else
-      flash[:message] = "Please log in to view the user."
-      redirect to '/login'
-    end
-  end
 
   get '/signup' do
     if !logged_in?
       erb :'users/create_user'
     else
-      redirect to '/users/:slug'
+      erb :'users/show'
     end
   end
 
-  post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
-      flash[:message] = "Please fill out all fields."
-      redirect to '/signup'
-    else
-      @creator = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-      @creator.save
-      session[:user_id] = @creator.id
-      flash[:message] = "You have successfully created an account!"
-      redirect to '/restaurants'
+    post '/signup' do
+      if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        flash[:message] = "Please fill out all fields."
+        redirect to '/signup'
+      else
+        @creator = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+        @creator.save
+        session[:user_id] = @creator.id
+        flash[:message] = "You have successfully created an account!"
+        redirect to '/restaurants'
+      end
     end
-  end
 
-  get '/login' do
-    if !logged_in?
-      erb :'users/login'
-    else
-      redirect to '/users/:slug'
+    get '/users/:slug' do
+      if logged_in?
+        @creator = User.find_by_slug(params[:slug])
+        erb :'users/show'
+      else
+        flash[:message] = "Please log in to view the user."
+        redirect to '/login'
+      end
     end
-  end
 
-  post '/login' do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect to "/users/:slug"
-    else
-      flash[:message] = "We couldn't find you. Click to sign up or check your username and password again."
-      redirect to "/signup"
+    get '/login' do
+      if !logged_in?
+        erb :'users/login'
+      else
+        erb :'users/show'
+      end
     end
-  end
 
-  get '/logout' do
-    if logged_in?
-      session.destroy
-      flash[:message] = "You have successfully logged out"
-      redirect to '/login'
-    else
-      redirect to '/'
+    post '/login' do
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        erb :'users/show'
+      else
+        flash[:message] = "We couldn't find you. Click to sign up or check your username and password again."
+        redirect to "/signup"
+      end
     end
+
+    get '/logout' do
+      if logged_in?
+        session.destroy
+        flash[:message] = "You have successfully logged out"
+        redirect to '/login'
+      else
+        redirect to '/'
+      end
+    end
+
   end
-end
