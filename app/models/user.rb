4f18a84f1ce3fd4a8c3a6d03aa329ca6f6a1b258
Allison Tzadok <allisontzadok@gmail.com>
2018-03-16 03:21:01 +0000

    updated all formatting for the create new restaurant page forthe 600 max screen

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,12 @@ class User < ActiveRecord::Base
   has_many :restaurants, through: :bookmarks
 
   has_secure_password
-  
+
   validates_uniqueness_of :username
 
 
   validates :username, length: { minimum: 3 }
   validates :password, length: { in: 6..20 }
-
-  validates_presence_of :username, message: "Username Required."
-  validates_presence_of  :password_digest, message: "Password Required."
-
 
 
 end
